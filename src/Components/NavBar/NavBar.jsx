daebac6c5251b7ee58fc69a7d85388bcfdd845cf
Syed Ashik Mahmud Pullock <pullock15-4981@diu.edu.bd>
2025-05-20 13:46:23 +0600

    add toggle btn in navbar

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -18,49 +18,123 @@ const NavBar = () => {
     </>
 
     // user info find from context
-    const userInfo = use(AuthContext);
-    console.log(userInfo);
+    const { user, SignOutUser } = use(AuthContext);
+    console.log('nav', user);
 
+    // sign out function
+    const handleSignOut = () => {
+        SignOutUser()
+            .then(() => {
+                console.log('User signed out successfully!');
+            })
+            .catch((error) => {
+                console.error('Error signing out:', error);
+            });
+    }
+
+
+    // return (
+    //     <div className=''>
+    //         <div className="navbar bg-base-100 shadow-sm  md:px-16 px-4">
+    //             <div className="navbar-start">
+    //                 <div className="dropdown">
+    //                     <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+    //                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+    //                     </div>
+    //                     <ul
+    //                         tabIndex={0}
+    //                         className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+    //                         {
+    //                             navLinks
+    //                         }
+    //                     </ul>
+    //                 </div>
+    //                 <div className="flex items-center gap-2">
+    //                     <img src={logo} alt="HobbyHub Logo" className="h-10" />
+    //                     <a className="font-bold text-xl md:block hidden">HobbyHub</a>
+    //                 </div>
+    //             </div>
+    //             <div className="navbar-center hidden lg:flex">
+    //                 <ul className="menu menu-horizontal px-1">
+    //                     {
+    //                         navLinks
+    //                     }
+    //                 </ul>
+    //             </div>
+    //             <div className="navbar-end">
+    //                 <Link to='/login'>
+    //                     <button className="btn bg-[#fdc323] font-bold">Login</button>
+    //                 </Link>
+    //                 <Link to='/register'>
+    //                     <button className="btn bg-[#fdc323] font-bold">Register</button>
+    //                 </Link>
+    //             </div>
+    //         </div>
+    //     </div>
+    // );
 
     return (
-        <div className=''>
-            <div className="navbar bg-base-100 shadow-sm  md:px-16 px-4">
-                <div className="navbar-start">
-                    <div className="dropdown">
-                        <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
-                        </div>
-                        <ul
-                            tabIndex={0}
-                            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-                            {
-                                navLinks
-                            }
-                        </ul>
+        <div className="navbar bg-base-100 shadow-sm md:px-10 px-4">
+            <div className="navbar-start">
+                <div className="dropdown">
+                    <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
                     </div>
-                    <div className="flex items-center gap-2">
-                        <img src={logo} alt="HobbyHub Logo" className="h-10" />
-                        <a className="font-bold text-xl md:block hidden">HobbyHub</a>
-                    </div>
-                </div>
-                <div className="navbar-center hidden lg:flex">
-                    <ul className="menu menu-horizontal px-1">
+                    <ul
+                        tabIndex={0}
+                        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+
                         {
                             navLinks
                         }
+
                     </ul>
                 </div>
-                <div className="navbar-end">
-                    <Link to='/login'>
-                        <button className="btn bg-[#fdc323] font-bold">Login</button>
-                    </Link>
-                    <Link to='/register'>
-                        <button className="btn bg-[#fdc323] font-bold">Register</button>
-                    </Link>
+                <div className="flex items-center gap-2">
+                    <img src={logo} alt="HobbyHub Logo" className="h-10" />
+                    <a className="font-bold text-xl md:block hidden">HobbyHub</a>
                 </div>
             </div>
-        </div>
+            <div className="navbar-center hidden lg:flex">
+                <ul className="menu menu-horizontal px-1">
+                    {
+                        navLinks
+                    }
+                </ul>
+            </div>
+
+            <div className="navbar-end">
+                {
+                    user ? (
+                        <div className="flex items-center gap-3 mr-2">
+                            <div className="tooltip tooltip-bottom" data-tip={user.displayName}>
+                                <img
+                                    src={user.photoURL}
+                                    alt="User Profile"
+                                    className="w-10 h-10 rounded-full border-2 border-gray-300 cursor-pointer"
+                                />
+                            </div>
+                            <button
+                                onClick={handleSignOut}
+                                className="btn bg-amber-400 font-bold"
+                            >
+                                Log out
+                            </button>
+                        </div>
+                    ) : (
+                        <Link to='/login'>
+                            <button className="btn bg-amber-400 font-bold">Log in</button>
+                        </Link>
+                    )
+                }
+            </div>
+
+
+        </div >
+
+
     );
+
 };
 
 export default NavBar;
